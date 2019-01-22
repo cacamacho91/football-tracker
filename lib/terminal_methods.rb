8e5183711a3cdc12028676d824d6e29b131c4234
Chris Camacho <cacamacho91@gmail.com>
2019-01-22 17:14:34 +0000

    added colour text support, hidden ids in terminal with black text on black background

--- lib/terminal_methods.rb
+++ lib/terminal_methods.rb
@@ -3,12 +3,19 @@
 
 #Takes a country and returns an array of strings containing all
 #  leagues that are availble for that country add the league ID on the end
+#  a hidden id is displayed within each input to enable compatability with TTY
 def show_league_by_country(chosen_country)
   country_leagues = get_leagues_by_country_name_and_season(chosen_country).values
-  country_leagues.map {|league| "#{league["name"]} ##{league["league_id"]}"}
+  country_leagues.map do |league|
+    hidden_id = Rainbow("##{league["league_id"]}#").black.bg(:black)
+    "#{league["name"]} #{hidden_id}"
+  end
 end
 
 def show_teams_by_league(chosen_league)
   league_teams = get_teams_by_league(chosen_league).values
-  league_teams.map {|team| "#{team["name"]} ##{team["team_id"]}"}
+  league_teams.map do |team|
+    hidden_id = Rainbow("##{team["team_id"]}#").black.bg(:black)
+    "#{team["name"]} #{hidden_id}"
+  end
 end
