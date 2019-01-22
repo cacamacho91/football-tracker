293ac3986df81fc059c1f5f9f03994e1cda4d233
Parik Panchal <flatiron@Flatirons-MacBook-Air-2.local>
2019-01-22 10:35:49 +0000

    base active-record setup

--- Gemfile
+++ Gemfile
@@ -1,7 +1,12 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-gem 'require_all'
 gem 'rest-client'
 gem 'json'
 gem 'tty-prompt'
+gem "sqlite3"
+gem "pry"
+gem 'sinatra'
+gem 'activerecord', :require => 'active_record'
+gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
+gem 'rake'
--- config/environment.rb
+++ config/environment.rb
@@ -2,10 +2,22 @@
 require 'bundler/setup'
 Bundler.require
 
-
 require_relative '../lib/api_communicator.rb'
 require_relative '../lib/command_line_interface.rb'
+
+# requiring all model classes
+require_relative '../app/models/country.rb'
+require_relative '../app/models/league.rb'
+require_relative '../app/models/user.rb'
+require_relative '../app/models/user_league.rb'
+
 
 #Set Environemnt Keys
 PROD_KEY = "HSrTEEejMlmshr6vg2SjrZpz5Nw1p1qGN6RjsnXZ8zGR0o4j5j"
 TEST_KEY = "O33pXS2rCjmsha0ZVTJZXNy3yCRsp1E9gY4jsn6OleTSPGrAKn"
+
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
