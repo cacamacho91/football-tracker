5973d9b06b9215d1461383a290787a202ac1f859
Chris Camacho <cacamacho91@gmail.com>
2019-01-22 10:46:51 +0000

    set up db with first create migration

---
+++ db/migrate/20190122104114_setting_up_users_leagues_countries_and_userleagues_tables.rb
@@ -0,0 +1,23 @@
+class SettingUpUsersLeaguesCountriesAndUserleaguesTables < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+    end
+
+    create_table :leagues do |t|
+      t.string :name
+      t.integer :country_id
+    end
+
+    create_table :countries do |t|
+      t.string :name
+    end
+
+    create_table :user_leagues do |t|
+      t.integer :user_id
+      t.integer :league_id
+    end
+  end
+end
