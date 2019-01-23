eecbd709a4ff611e695619979aa380249ef616c8
Parik Panchal <flatiron@Flatirons-MacBook-Air-2.local>
2019-01-23 11:40:42 +0000

    Created seed data, created statistics methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ActiveRecord::Base
   has_many :user_teams
   has_many :leagues, through: :user_leagues
   has_many :teams, through: :user_teams
+
+  # USER METHODS
+
+  def find_users_that_support_my_teams
+  array = self.teams.collect{|team| team.id}
+  all_users = array.map {|team_id| Team.find(team_id).users}.flatten
+  all_users.uniq.drop(1)
+  end
+
+
+  def find_users_that_support_my_leagues
+  array = self.leagues.collect{|league| league.id}
+  all_users = array.map {|league_id| League.find(league_id).users}.flatten
+  all_users.uniq.drop(1)
+  end
+
+  def suggest_teams
+  all_users = self.teams.map {|team| team.users}.flatten.flatten.uniq.drop(1)
+  all_users_array = all_users.collect{|user| user.teams}
+  end
+
+
+  def suggest_leagues
+  all_users = self.leagues.map {|league| league.users}.flatten.uniq.drop(1)
+  all_users_array = all_users.collect{|user| user.leagues}.flatten
+  end
+  
+  #class end
 end
---
+++ stats_methods.rb
@@ -0,0 +1,51 @@
+
+# THESE ARE ALL THE STATISTICS METHODS
+
+def user_who_supports_most_teams
+  array = UserTeam.all.collect{|user| user.user_id}
+  most_common_value = array.uniq.max_by{ |i| array.count( i ) }
+  user = User.find(most_common_value)
+user.name
+end
+
+
+def user_who_supports_most_leagues
+  array = UserLeague.all.collect{|user| user.user_id}
+  most_common_value = array.uniq.max_by{ |i| array.count( i ) }
+  user = User.find(most_common_value)
+user.name
+end
+
+
+def team_with_most_followers
+  array = UserTeam.all.collect{|user| user.team_id}
+    most_common_value = array.uniq.max_by{ |i| array.count( i ) }
+    team = Team.find(most_common_value)
+team.api_team_id
+end
+
+
+
+def team_with_least_followers
+  array = UserTeam.all.collect{|user| user.team_id}
+  most_common_value = array.uniq.min_by{ |i| array.count( i ) }
+  team = Team.find(most_common_value)
+team.api_team_id
+end
+
+
+def league_with_most_followers
+  array = UserLeague.all.collect{|user| user.league_id}
+  most_common_value = array.uniq.max_by{ |i| array.count( i ) }
+  league = League.find(most_common_value)
+league.api_league_id
+end
+
+
+
+def league_with_least_followers
+  array = UserLeague.all.collect{|user| user.league_id}
+  most_common_value = array.uniq.min_by{ |i| array.count( i ) }
+  league = League.find(most_common_value)
+league.api_league_id
+end
